9f8e2efb17947452598bacaf87410e0ce75fe155
Adam Lawson <dev@goshawk22.uk>
2022-03-13 16:49:03 +0000

    Update for Bibble Hard

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -11,7 +11,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         In Bibble, try to guess the word from the King James V Bible.
-        Guess the word in 6 tries. After each guess, the color of the tiles will
+        Guess the word in 10 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
 
@@ -19,40 +19,46 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="P"
           status="correct"
         />
-        <Cell value="E" />
+        <Cell value="O" />
+        <Cell value="P" />
+        <Cell value="U" />
+        <Cell value="L" />
         <Cell value="A" />
         <Cell value="R" />
-        <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter P is in the word and in the correct spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="C" />
+        <Cell value="O" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="R"
+          status="present"
+        />
+        <Cell value="O" />
+        <Cell value="N" />
+        <Cell value="E" />
+        <Cell value="R" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter R is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="P" />
-        <Cell value="I" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="L"
-          status="present"
-        />
-        <Cell value="O" />
-        <Cell value="T" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
         <Cell value="E" />
+        <Cell value="T" />
+        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="N" />
+        <Cell value="I" />
+        <Cell value="A" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter U is not in the word in any spot.
